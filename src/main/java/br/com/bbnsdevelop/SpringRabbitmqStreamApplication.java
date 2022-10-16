f64f2e851bb9de6fe452420ae82648d2d2441eb1
bbnsdevelop <bbnsdevelop@gmail.com>
2022-10-16 15:32:40 -0300

    creating project

---
+++ src/main/java/br/com/bbnsdevelop/SpringRabbitmqStreamApplication.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitmqStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bbnsdevelop/SpringRabbitmqStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bbnsdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
